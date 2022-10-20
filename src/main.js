88d9f14879d68d024bf6da3e8e486cd223fa5264
rafaelmpardim <rafaelmedeirospardim@gmail.com>
2022-10-20 13:12:45 -0300

    feat: class 02: professionalizing your project

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+import IMask from "imask"
+
 import "./css/index.css"
 
 const ccBgColor01 = document.querySelector('.cc-bg svg > g g:nth-child(1) path')
@@ -6,9 +8,9 @@ const ccLogo = document.querySelector('.cc-logo span:nth-child(2) img')
 
 function setCardType(type) {
   const colors = {
-    visa: ['#436D99', '#2D57F2'],
+    default: ['black', 'gray'],
     mastercard: ['#DF6F29', '#C69347'],
-    default: ['black', 'gray']
+    visa: ['#436D99', '#2D57F2']
   }
 
   ccBgColor01.setAttribute('fill', colors[type][0])
@@ -17,3 +19,68 @@ function setCardType(type) {
 }
 
 globalThis.setCardType = setCardType
+
+const cardControll = {
+  expirationDate: {
+    input: document.getElementById('expiration-date'),
+    pattern: {
+      mask: 'MM{/}YY',
+      blocks: {
+        MM: {
+          mask: IMask.MaskedRange,
+          from: 1,
+          to: 12
+        },
+        YY: {
+          mask: IMask.MaskedRange,
+          from: String(new Date().getFullYear()).slice(2),
+          to: String(new Date().getFullYear() + 10 ).slice(2) 
+        }
+      },
+    }
+  },
+  holder: {
+    input: document.getElementById('card-holder'),
+    pattern: {}
+  },
+  number: {
+    input: document.getElementById('card-number'),
+    pattern: {
+      mask: [
+        {
+          mask: '0000 0000 0000 0000',
+          cardtype: 'visa',
+          regex: /^4\d{0,15}/
+        },
+        {
+          mask: '0000 0000 0000 0000',
+          cardtype: 'mastercard',
+          regex: /(^5[1-5]\d{0,2}|^22[2-9]\d|^2[3-7]\d{0,2})\d{0,12}/
+        },
+        {
+          mask: '0000 0000 0000 0000',
+          cardtype: 'default'
+        }
+      ],
+      dispatch: function (appended, dynamicMasked) {
+        let number = (dynamicMasked.value + appended).replace(/\D/g, '')
+        const foundMask = dynamicMasked.compiledMasks.find(({ regex }) => number.match(regex))
+
+        return foundMask
+      }
+    }
+  },
+  securityCode: {
+    input: document.getElementById('security-code'),
+    pattern: {
+      mask: '0000'
+    }
+  }
+}
+
+const cardExpirationDateMasked = IMask(cardControll.expirationDate.input, cardControll.expirationDate.pattern)
+// const cardHolderMasked = IMask(cardControll.holder.input, cardControll.holder.pattern)
+const cardNumberMasked = IMask(cardControll.number.input, cardControll.number.pattern)
+const cardSecutityCodeMasked = IMask(cardControll.securityCode.input, cardControll.securityCode.pattern)
+
+console.log(cardSecutityCodeMasked)
